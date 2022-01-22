c4c5022cb15575a0c0561fa2ab1145274acfaffb
RaINi_ <razor13372k@gmail.com>
2022-01-22 02:31:48 +0100

    feat: proper real ip extractor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_files::NamedFile;
-use actix_governor::{Governor, KeyExtractor, GovernorConfigBuilder};
-use actix_web::{error, middleware, web, App, HttpServer, Result, dev::ServiceRequest};
+use actix_governor::{Governor, GovernorConfigBuilder, KeyExtractor};
+use actix_web::{dev::ServiceRequest, error, middleware, web, App, HttpServer, Result};
 use chrono::{SubsecRound, Utc};
 use clap::Parser;
 use serde::Deserialize;
@@ -115,13 +115,49 @@ pub struct RealIpKeyExtractor;
 
 impl KeyExtractor for RealIpKeyExtractor {
     type Key = IpAddr;
-    type KeyExtractionError = std::net::AddrParseError;
+    type KeyExtractionError = &'static str;
+
+    #[cfg(feature = "log")]
+    fn name(&self) -> &'static str {
+        "real IP"
+    }
 
     fn extract(&self, req: &ServiceRequest) -> Result<Self::Key, Self::KeyExtractionError> {
-        req.connection_info()
-            .realip_remote_addr()
-            .unwrap_or("")
-            .parse::<IpAddr>()
+        // Get the reverse proxy IP that we put in app data
+        let reverse_proxy_ip = req
+            .app_data::<web::Data<IpAddr>>()
+            .map(|ip| ip.get_ref().to_owned())
+            .unwrap_or_else(|| IpAddr::from_str("0.0.0.0").unwrap());
+
+        let peer_ip = req.peer_addr().map(|socket| socket.ip());
+        let connection_info = req.connection_info();
+
+        match peer_ip {
+            // The request is coming from the reverse proxy, we can trust the `Forwarded` or `X-Forwarded-For` headers
+            Some(peer) if peer == reverse_proxy_ip => connection_info
+                .realip_remote_addr()
+                .ok_or("Could not extract real IP address from request")
+                .and_then(|str| {
+                    SocketAddr::from_str(str)
+                        .map(|socket| socket.ip())
+                        .or_else(|_| IpAddr::from_str(str))
+                        .map_err(|_| "Could not extract real IP address from request")
+                }),
+            // The request is not comming from the reverse proxy, we use peer IP
+            _ => connection_info
+                .remote_addr()
+                .ok_or("Could not extract peer IP address from request")
+                .and_then(|str| {
+                    SocketAddr::from_str(str)
+                        .map_err(|_| "Could not extract peer IP address from request")
+                })
+                .map(|socket| socket.ip()),
+        }
+    }
+
+    #[cfg(feature = "log")]
+    fn key_name(&self, key: &Self::Key) -> Option<String> {
+        Some(key.to_string())
     }
 }
 
